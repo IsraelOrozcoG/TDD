194a21da35da1551b1e1d06f128619b96b9ecbf9
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2022-04-28 15:02:54 -0500

    Se creo el proyecto twitter,carpetas app/models , test/,models archivo package.json, index.js, User.js y User.test.js

---
+++ twitter/test/models/User.test.js
@@ -0,0 +1,18 @@
+const User = require ('./../../app/models/user')
+
+describe("Unit Test for User class", ()=>{
+
+    test('Create an User object', () =>{
+
+    const user =new User (1,"carlogilmar","carlo","Bio","dateCreated","lastUpdated")
+
+
+    expect(user.id).toBe(1)
+    expect(user.username).toBe("carlogilmar")
+    expect(user.name).toBe("carlo")
+    expect(user.bio).toBe("Bio")
+    expect(user.dateCreated).toBe("dateCreated")
+    expect(user.lastUpdated).toBe("lastUpdated")
+
+});
+})
