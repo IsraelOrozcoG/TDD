2162f2507fb56d7bd18a913d82fd2da9f3ed3819
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2022-04-28 15:26:57 -0500

    User.test.js modificado para dateCreated y lastUpdated

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -4,15 +4,15 @@ describe("Unit Test for User class", ()=>{
 
     test('Create an User object', () =>{
 
-    const user =new User (1,"carlogilmar","carlo","Bio","dateCreated","lastUpdated")
+    const user =new User (1,"carlogilmar","carlo","Bio")
 
 
     expect(user.id).toBe(1)
     expect(user.username).toBe("carlogilmar")
     expect(user.name).toBe("carlo")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined()
 
 });
 })
